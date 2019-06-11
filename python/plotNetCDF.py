4e2372aa1b0b9cafca279eb0970122008343e2a0
Peter Jansen <peter.jansen@csiro.au>
2019-06-11 19:03:04 +1000

    Commit plotting upgrades to python3

--- python/plotNetCDF.py
+++ python/plotNetCDF.py
@@ -1,6 +1,5 @@
 from netCDF4 import Dataset
 from netCDF4 import num2date
-
 import datetime as dt
 import numpy as np
 import matplotlib
@@ -12,101 +11,26 @@ import sys
 import os
 from matplotlib import rc
 
-
-def ncdump(nc_fid, verb=True):
-    '''
-    ncdump outputs dimensions, variables and their attribute information.
-    The information is similar to that of NCAR's ncdump utility.
-    ncdump requires a valid instance of Dataset.
-
-    Parameters
-    ----------
-    nc_fid : netCDF4.Dataset
-        A netCDF4 dateset object
-    verb : Boolean
-        whether or not nc_attrs, nc_dims, and nc_vars are printed
-
-    Returns
-    -------
-    nc_attrs : list
-        A Python list of the NetCDF file global attributes
-    nc_dims : list
-        A Python list of the NetCDF file dimensions
-    nc_vars : list
-        A Python list of the NetCDF file variables
-    '''
-
-    def print_ncattr(key):
-        """
-        Prints the NetCDF file attributes for a given key
-
-        Parameters
-        ----------
-        key : unicode
-            a valid netCDF4.Dataset.variables key
-        """
-        try:
-            print ("\t\ttype: ", repr(nc_fid.variables[key].dtype))
-            for ncattr in nc_fid.variables[key].ncattrs():
-                print ('\t\t%s:' % ncattr, \
-                    repr(nc_fid.variables[key].getncattr(ncattr)))
-        except KeyError:
-            print ("\t\tWARNING: %s does not contain variable attributes" % key)
-
-    # NetCDF global attributes
-    nc_attrs = nc_fid.ncattrs()
-    if verb:
-        print ("NetCDF Global Attributes:")
-        for nc_attr in nc_attrs:
-            print ('\t%s:' % nc_attr, repr(nc_fid.getncattr(nc_attr)))
-    nc_dims = [dim for dim in nc_fid.dimensions]  # list of nc dimensions
-    # Dimension shape information.
-    if verb:
-        print ("NetCDF dimension information:")
-        for dim in nc_dims:
-            print ("\tName:", dim)
-            print ("\t\tsize:", len(nc_fid.dimensions[dim]))
-            print_ncattr(dim)
-    # Variable information.
-    nc_vars = [var for var in nc_fid.variables]  # list of nc variables
-    if verb:
-        print "NetCDF variable information:"
-        for var in nc_vars:
-            if var not in nc_dims:
-                print ('\tName:', var)
-                print ("\t\tdimensions:", nc_fid.variables[var].dimensions)
-                print ("\t\tsize:", nc_fid.variables[var].size)
-                print_ncattr(var)
-    return nc_attrs, nc_dims, nc_vars
-
-
 # rc('text', usetex=True)
 
 for path_file in sys.argv[1:len(sys.argv)]:
 
     nc = Dataset(path_file)
-
-    nc_attrs, nc_dims, nc_vars = ncdump(nc)
+    nc_dims = [dim for dim in nc.dimensions]  # list of nc dimensions
 
     nctime = nc.variables['TIME'][:]
     t_unit = nc.variables['TIME'].units  # get unit  "days since 1950-01-01T00:00:00Z"
 
     try:
-
         t_cal = nc.variables['TIME'].calendar
 
     except AttributeError:  # Attribute doesn't exist
-
         t_cal = u"gregorian"  # or standard
-
-    t1 = num2date(nctime[0], units=t_unit, calendar=t_cal)
-
-    print ('time ', t1)
 
     dt_time = [num2date(t, units=t_unit, calendar=t_cal) for t in nctime]
 
     # remove any dimensions from the list to plot
-    nc_vars_to_plot = nc_vars;
+    nc_vars_to_plot = [var for var in nc.variables]
     for i in nc_dims:
         try:
             nc_vars_to_plot.remove(i)
@@ -122,7 +46,7 @@ for path_file in sys.argv[1:len(sys.argv)]:
             pass
 
     # remove any variables without a TIME dimension from the list to plot
-    to_plot = list();
+    to_plot = list()
 
     for var in nc.variables:
         # print var
@@ -131,7 +55,7 @@ for path_file in sys.argv[1:len(sys.argv)]:
         if var in aux_vars:
             continue
         if 'TIME' in nc.variables[var].dimensions:
-            print ('to plot ', var)
+            print('to plot ', var)
             to_plot.append(var)
 
     # pdffile = path_file[path_file.rfind('/')+1:len(path_file)] + '-' + nc.getncattr('deployment_code') + '-plot.pdf'
@@ -145,8 +69,8 @@ for path_file in sys.argv[1:len(sys.argv)]:
     text = 'file name : ' + os.path.basename(path_file) + '\n'
 
     # print "NetCDF Global Attributes:"
-    for nc_attr in nc_attrs:
-        # print ('\t%s:' % nc_attr, repr(nc.getncattr(nc_attr)))
+    for nc_attr in nc.ncattrs():
+        # print '\t%s:' % nc_attr, repr(nc.getncattr(nc_attr))
         text += nc_attr + ' : ' + str(nc.getncattr(nc_attr)) + '\n'
 
     plt.text(-0.1, -0.1, text, fontsize=8, family='monospace')
@@ -167,11 +91,8 @@ for path_file in sys.argv[1:len(sys.argv)]:
         # print "NetCDF Variable Attributes:"
         for nc_attr in nc_attrs:
             attrVal = plot_var.getncattr(nc_attr)
-            print ('\t%s:' % nc_attr, repr(plot_var.getncattr(nc_attr)), type(attrVal))
-            if type(attrVal) != unicode:
-                text += nc_attr.encode('utf-8') + ' : ' + str(attrVal) + '\n'
-            else:
-                text += nc_attr.encode('utf-8') + ' : ' + attrVal + '\n'
+            print('\t%s:' % nc_attr, repr(plot_var.getncattr(nc_attr)), type(attrVal))
+            text += nc_attr + ' : ' + str(attrVal) + '\n'
 
         if hasattr(plot_var, 'ancillary_variables'):
             qc_var_name = plot_var.getncattr('ancillary_variables')
@@ -199,7 +120,7 @@ for path_file in sys.argv[1:len(sys.argv)]:
         pp.savefig(fig)
         plt.close(fig)
 
-        print (plot_var.name, " shape ", var.shape, " len ", shape_len)
+        print(plot_var.name, " shape ", var.shape, " len ", shape_len)
 
         fig = plt.figure(figsize=(11.69, 8.27))
         if plot_var.dimensions[0] != 'TIME':
@@ -211,38 +132,42 @@ for path_file in sys.argv[1:len(sys.argv)]:
         mi = qc_m.min()
 
         marg = (mx - mi) * 0.1
-        print ("max ", mx, " min ", mi)
+        print("max ", mx, " min ", mi)
 
         plt.ylim([mi - marg, mx + marg])
 
         if hasattr(plot_var, 'sensor_serial_number'):
             sn = plot_var.getncattr('sensor_serial_number').split('; ')
         else:
-            sn = nc.getncattr('instrument_serial_number')
+            sn = nc.getncattr('instrument_serial_number').split('; ')
 
         if hasattr(plot_var, 'sensor_depth'):
             dpth = plot_var.getncattr('sensor_depth').split('; ')
         elif hasattr(plot_var, 'sensor_height'):
             dpth = plot_var.getncattr('sensor_height').split('; ')
+        elif hasattr(nc, 'instrument_nominal_depth'):
+            dpth = str(nc.getncattr('instrument_nominal_depth')).split('; ')
         else:
             dpth = 'unknown'
+
+        print("depth ", dpth)
 
         leg = [x + ' (' + y + ' m)' for x, y in zip(sn, dpth)]
 
         plot_marks = '-'
         if len(dt_time) < 200:
             plot_marks = '.-'
+            
         pl = plt.plot(dt_time, qc_m, plot_marks)
-        # plt.legend(iter(pl), leg)
-        plt.legend(iter(pl), leg, bbox_to_anchor=(0.0, -0.2, 1.0, -0.15), loc=3, ncol=6, mode="expand",
-                   borderaxespad=0.0, fontsize='x-small')
+        plt.legend(iter(pl), leg)
+        #plt.legend(iter(pl), leg, bbox_to_anchor=(0.0, -0.2, 1.0, -0.15), loc=3, ncol=6, mode="expand",   borderaxespad=0.0, fontsize='x-small')
 
-        qc_m = np.ma.masked_where(qc <= 1, var)
+        qc_m = np.ma.masked_where((qc <= 2) | (qc == 8), var)
         plt.plot(dt_time, qc_m, 'yo')
-        qc_m = np.ma.masked_where(qc <= 3, var)
+        qc_m = np.ma.masked_where((qc <= 3) | (qc == 8), var)
         plt.plot(dt_time, qc_m, 'ro')
 
-        fig.autofmt_xdate()
+        #fig.autofmt_xdate()
         plt.grid()
 
         # add deployment/instrument/standard name as title
@@ -251,7 +176,10 @@ for path_file in sys.argv[1:len(sys.argv)]:
         #          plot_var.sensor_serial_number + ' : ' + plot_var.name, fontsize=10)
 
         # plt.title(nc.getncattr('deployment_code') + ' : ' + plot_var.getncattr('name'), fontsize=10)
-        plt.title(nc.getncattr('deployment_code'), fontsize=10)
+        try:
+            plt.title(nc.getncattr('deployment_code'), fontsize=10)
+        except AttributeError:
+            pass
 
         # add units to Y axis
 
