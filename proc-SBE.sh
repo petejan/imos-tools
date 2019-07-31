50876ca84666fd5ae4e307cfac9c974e1fb25fc5
Peter Jansen <peter.jansen@csiro.au>
2019-07-31 18:34:04 +1000

    changes from directory renaming

--- proc-SBE.sh
+++ proc-SBE.sh
@@ -2,10 +2,10 @@
 
 file=data/SBE37-1777-1000m.asc
 
-python3 python/file-parsing/sbeASC2netCDF.py ${file}
-python3 python/processing/addPSAL.py ${file}.nc
-python3 python/attribution/addAttributes.py ${file}.nc metadata/imos.metadata.csv metadata/SAZ47-20-2018.metadata.csv metadata/SAZ47-20-2018-instrument.metadata.csv metadata/sots.metadata.csv metadata/variable.metadata.csv
-python3 python/attribution/add_geospatial_attributes.py ${file}.nc
-python3 python/plotting/plotNetCDF.py ${file}.nc
+python3 ocean_dp/parse/sbeASC2netCDF.py ${file}
+python3 ocean_dp/processing/addPSAL.py ${file}.nc
+python3 ocean_dp/attribution/addAttributes.py ${file}.nc metadata/imos.metadata.csv metadata/SAZ47-20-2018.metadata.csv metadata/SAZ47-20-2018-instrument.metadata.csv metadata/sots.metadata.csv metadata/variable.metadata.csv
+python3 ocean_dp/attribution/add_geospatial_attributes.py ${file}.nc
+python3 ocean_dp/plotting/plotNetCDF.py ${file}.nc
 
-python3 python/file-naming/imosNetCDFfileName.py ${file}.nc
+python3 ocean_dp/file_naming/imosNetCDFfileName.py ${file}.nc
--- procAQD.sh
+++ procAQD.sh
@@ -2,22 +2,22 @@
 
 file=data/SAZ20-2018.aqd
 
-python3 python/file-parsing/nortek2netCDF.py $file
-python3 python/attribution/addAttributes.py $file.nc metadata/SAZ47-20.metadata.csv metadata/imos.metadata.csv metadata/SAZ47.metadata.csv metadata/variable.metadata.csv
-python3 python/processing/add_mag_variation.py $file.nc 
-python3 python/attribution/add_geospatial_attributes.py $file.nc
+python3 ocean_dp/parse/nortek2netCDF.py $file
+python3 ocean_dp/attribution/addAttributes.py $file.nc metadata/SAZ47-20.metadata.csv metadata/imos.metadata.csv metadata/SAZ47.metadata.csv metadata/variable.metadata.csv
+python3 ocean_dp/processing/add_mag_variation.py $file.nc 
+python3 ocean_dp/attribution/add_geospatial_attributes.py $file.nc
 
-python3 python/processing/findDeploymentRecoveryTimes.py $file.nc
+python3 ocean_dp/processing/findDeploymentRecoveryTimes.py $file.nc
 
-fn=$(python3 python/file-naming/imosNetCDFfileName.py $file.nc)
+fn=$(python3 ocean_dp/file_name/imosNetCDFfileName.py $file.nc)
 
-python3 python/processing/magnetic_to_true.py $fn
+python3 ocean_dp/processing/magnetic_to_true.py $fn
 
-fn=$(python3 python/data-qc/add_qc_flags.py $fn)
-python3 python/data-qc/in_out_water.py $fn
-python3 python/data-qc/finalize_qc.py $fn
-python3 python/attribution/format_attributes.py $fn 
+fn=$(python3 ocean_dp/qc/add_qc_flags.py $fn)
+python3 ocean_dp/qc/in_out_water.py $fn
+python3 ocean_dp/qc/finalize_qc.py $fn
+python3 ocean_dp/attribution/format_attributes.py $fn 
 
-# python3 python/plotting/plotNetCDF.py $fn
+# python3 ocean_dp/plotting/plotNetCDF.py $fn
 
 #echo $fn
