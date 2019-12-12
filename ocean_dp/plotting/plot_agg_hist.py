bac39c26a468ff55d2967aaa1684772a304b884d
Peter Jansen <peter.jansen@csiro.au>
2019-12-13 09:27:44 +1100

    Create plot_agg_hist.py
    plotting of aggregate file depth distogram

---
+++ ocean_dp/plotting/plot_agg_hist.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+# plot_binned
+# Copyright (C) 2019 Peter Jansen
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
+from netCDF4 import Dataset, num2date, chartostring
+from dateutil import parser
+from datetime import datetime
+from datetime import timedelta
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import sys
+
+
+def plot_binned(netCDFfiles):
+    ds = Dataset(netCDFfiles[1], 'r')
+
+    depth = ds.variables["DEPTH"]
+
+    d = depth[:]
+
+    h = np.histogram(d, bins=np.arange(0, 3500, 1))
+    plt.plot(h[0], h[1][0:-1], '.-')
+    plt.gca().invert_yaxis()
+    plt.grid(True)
+    plt.show()
+
+    ds.close()
+
+if __name__ == "__main__":
+    plot_binned(sys.argv)
+
