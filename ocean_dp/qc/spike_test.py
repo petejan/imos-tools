42ef01b2480a95c3e96ffed5778c4a2e09d19313
bweeding <57697604+bweeding@users.noreply.github.com>
2020-02-24 12:16:56 +1100

    test changes
    1st write of spike test, alter name=main  and history for flatline

--- ocean_dp/qc/spike_test.py
+++ ocean_dp/qc/spike_test.py
@@ -28,13 +28,13 @@ import os
 
 
 # If files aren't specified, take all the IMOS*.nc files in the current folder
-def flatline_test_all_files(target_vars_in=[], window=3, flag=3):
+def spike_test_all_files(target_vars_in=[], thresh_low=2, thresh_high=4, flag_low=3, flag_high=4):
     target_files = glob.glob('IMOS*.nc')
 
-    flatline_test_files(target_files, target_vars_in=target_vars_in, window=window, flag=flag)
+    spike_test_files(target_files, target_vars_in=target_vars_in, thresh_low=thresh_low,thresh_high=thresh_high,flag_low=flag_low, flag_high=flag_high)
 
 
-def flatline_test_files(target_files, target_vars_in=[], window=3, flag=3):
+def spike_test_files(target_files, target_vars_in=[], thresh_low=2, thresh_high=4, flag_low=3, flag_high=4):
     
     # Loop through each files in target_files
     for current_file in target_files:
@@ -44,11 +44,11 @@ def flatline_test_files(target_files, target_vars_in=[], window=3, flag=3):
         # Extract netcdf data into nc
         nc = Dataset(current_file, mode="a")
 
-        # run the flat line test
-        flatline_test(nc=nc, target_vars_in=target_vars_in, window=window, flag=flag)
+        # run the spike test
+        spike_test(nc=nc, target_vars_in=target_vars_in, thresh_low=thresh_low,thresh_high=thresh_high,flag_low=flag_low, flag_high=flag_high)
 
 
-def flatline_test(nc, target_vars_in=[], window=3, flag=3):
+def spike_test(nc, target_vars_in=[], thresh_low=2, thresh_high=4, flag_low=3, flag_high=4):
     
         # If target_vars aren't user specified, set it to all the variables of 
         # the current_file, removing unwanted variables
@@ -79,14 +79,24 @@ def flatline_test(nc, target_vars_in=[], window=3, flag=3):
             
             print('checking '+current_var)
             
-            # Step through the data, one element at a time, using the window
-            for i in range(0,(len(var_data)-window+1)):
+            # Step through the data, one element at a time, starting from the 2nd element
+            for i in range(1,(len(var_data)-1)):
                 
-                # This is true if 'window' elements in a row are equal
-                if len(set(var_data[i:(i+window)])) == 1:
+                # Calculate the mean of the i-1 and i+1 elements
+                shoulder_mean = np.mean(np.take(var_data,[i-1,i+1]))
+                
+                # Check for spike exceeding high threshold
+                if abs(var_data[i]-shoulder_mean) > thresh_high:
+                    
+                    #set corresponding QC value to...
+                    nc.variables[current_var+'_quality_control'][i] = flag_high
+
+                
+                # Check for spike exceeding low threshold
+                elif abs(var_data[i]-shoulder_mean) > thresh_low:
                     
                     # set corresponding QC value to...
-                    nc.variables[current_var+'_quality_control'][i:(i+window)] = flag
+                    nc.variables[current_var+'_quality_control'][i] = flag_low
 
         # update the history attribute
         try:
@@ -94,10 +104,17 @@ def flatline_test(nc, target_vars_in=[], window=3, flag=3):
         except AttributeError:
             hist = ""
 
-        nc.setncattr('history', hist + datetime.utcnow().strftime("%Y-%m-%d") + 'flatline_test performed on [' + str(target_vars) + '], window '+str(window)+' consecutive values or more were flagged with '+str(flag) )
+        nc.setncattr('history', hist + datetime.utcnow().strftime("%Y-%m-%d") + ' :spike_test performed on [' + str(target_vars) + '], with spikes greater than '+str(thresh_high)+' flagged as '+str(flag_high)+' and spikes greater than '+str(thresh_low)+' flagged as '+str(flag_low))
 
         nc.close()
     
 if __name__ == "__main__":
     # usage is <file_name> <variable_name> <window> <flag value>
-    flatline_test_files(target_files=[sys.argv[1]], target_vars_in=[sys.argv[2]], window=float(sys.argv[3]), flag=float(sys.argv[4]))
+    spike_test_files(target_files=[sys.argv[1]], target_vars_in=[sys.argv[2]], thresh_low=float(sys.argv[3]), thresh_high=float(sys.argv[4]), flag_low= float(sys.argv[5]), flag_high= float(sys.argv[6]))
+
+    
+
+
+
+
+
