d50bfa0fb02e068f78e4f12c2b50f2742ed7b56c
Peter Jansen <peter.jansen@csiro.au>
2020-04-16 17:20:24 +1000

    Update add_incoming_radiation.py
    adding altitude and ePAR scalled by depth kd, still have speed issues, need to make kd a input attribute

--- ocean_dp/processing/add_incoming_radiation.py
+++ ocean_dp/processing/add_incoming_radiation.py
@@ -26,66 +26,127 @@ from pysolar.solar import get_altitude
 from pysolar.util import extraterrestrial_irrad
 from pysolar.util import global_irradiance_overcast
 
-import pysolar
-
-# add incoming radiation
-
-def add_solar(netCDFfile):
-    ds = Dataset(netCDFfile, 'a')
-
-    lat = ds.variables['LATITUDE'][:]
-    lon = ds.variables['LONGITUDE'][:]
-
-    print('lat ', lat, ' lon ', lon)
-
-    time = ds.variables['TIME']
-
-    print('number of points ', len(time))
-    dt = num2date(time[:], units=time.units, calendar=time.calendar)
-
-    dt_utc = [d.replace(tzinfo=pytz.UTC) for d in dt]
-
-    print('time ', dt_utc[0])
-
-    altitude_deg = [get_altitude(lat, lon, d) for d in dt_utc]
-    rad = [extraterrestrial_irrad(lat, lon, d, 1370) for d in dt_utc]
-
-    print("altitude", altitude_deg[0], " rad ", rad[0])
-
-    ncVarOut = ds.createVariable("ALT", "f4", ("TIME",), fill_value=np.nan, zlib=True)  # fill_value=nan otherwise defaults to max
-    ncVarOut[:] = altitude_deg
-    ncVarOut.units = "deg"
-    ncVarOut.setncattr('name', 'sun altitude')
-    ncVarOut.long_name = 'sun_altitude'
-    ncVarOut.comment = "using http://docs.pysolar.org/en/latest/ v0.8 get_altitude"
-
-    ncVarOut = ds.createVariable("SOLAR", "f4", ("TIME",), fill_value=np.nan, zlib=True)  # fill_value=nan otherwise defaults to max
-    ncVarOut[:] = rad
-    ncVarOut.units = "W/m2"
-    ncVarOut.setncattr('name', 'extraterrestrial_irrad celestial incoming solar radiation')
-    ncVarOut.long_name = 'incoming_solar_radiation'
-    ncVarOut.comment = "using http://docs.pysolar.org/en/latest/ v0.8 extraterrestrial_irrad() with incoming = 1370 W/m^2"
-
-    rad = [global_irradiance_overcast(lat, lon, d, 1370, temperature=10) for d in dt_utc]
-
-    ncVarOut = ds.createVariable("SURFACE", "f4", ("TIME",), fill_value=np.nan, zlib=True)  # fill_value=nan otherwise defaults to max
-    ncVarOut[:] = rad
-    ncVarOut.units = "W/m2"
-    ncVarOut.setncattr('name', 'extraterrestrial_irrad celestial incoming solar radiation')
-    ncVarOut.long_name = 'incoming_solar_radiation'
-    ncVarOut.comment = "using http://docs.pysolar.org/en/latest/ v0.8 global_irradiance_overcast() with incoming = 1370 W/m^2"
+import os
+import shutil
 
 
-    # update the history attribute
-    try:
-        hist = ds.history + "\n"
-    except AttributeError:
-        hist = ""
+def add_solar(netCDFfiles):
+    # add incoming radiation
 
-    ds.setncattr('history', hist + datetime.utcnow().strftime("%Y-%m-%d") + " : added incoming radiation")
+    out_files = []
 
-    ds.close()
+    for fn in netCDFfiles:
+        # Change the creation date in the filename to today
+        now = datetime.utcnow()
+
+        fn_new = fn
+        if os.path.basename(fn).startswith("IMOS_"):
+            fn_new_split = os.path.basename(fn).split('_')
+            fn_new_split[-1] = "C-" + now.strftime("%Y%m%d") + ".nc"
+            try:
+                fn_new_split[2].index("X") # for want of a better code
+            except ValueError:
+                fn_new_split[2] += 'X'
+
+            fn_new = os.path.join(os.path.dirname(fn), '_'.join(fn_new_split))
+
+        # If a new (different) filename has been successfully generated, make
+        # a copy of the old file with the new name
+        if fn_new != fn:
+            print('copying file to ', fn_new)
+            # copy file
+            shutil.copy(fn, fn_new)
+
+        out_files.append(fn_new)
+
+        ds = Dataset(fn_new, 'a')
+
+        lat = ds.variables['LATITUDE'][:]
+        lon = ds.variables['LONGITUDE'][:]
+
+        print('lat ', lat, ' lon ', lon)
+
+        time_var = ds.variables['TIME']
+        depth_var = ds.variables['PRES']
+        depth = depth_var[:]
+
+        print('number of points ', len(time_var))
+        dt = num2date(time_var[:], units=time_var.units, calendar=time_var.calendar, only_use_cftime_datetimes=False)
+
+        dt_utc = [d.replace(tzinfo=pytz.UTC) for d in dt]
+
+        print('time start ', dt_utc[0])
+
+        #lat_array = np.full_like(ds.variables['TIME'][:], lat)
+        #lon_array = np.full_like(ds.variables['TIME'][:], lon)
+
+        altitude_deg = [get_altitude(lat, lon, d) for d in dt_utc]
+        rad = [extraterrestrial_irrad(lat, lon, d, 1361) for d in dt_utc]
+
+        #altitude_deg = get_altitude(lat_array, lon_array, dt)
+        #rad = extraterrestrial_irrad(lat_array, lon_array, dt_utc, 1361)
+
+        #print("rad ", rad)
+        #print("depth ", depth)
+        par = rad * np.exp(-0.04 * depth) * 2.114
+
+        print("altitude", altitude_deg[0], " rad ", rad[0])
+
+        if 'ALT' in ds.variables:
+            ncVarOut = ds.variables["ALT"]
+        else:
+            ncVarOut = ds.createVariable("ALT", "f4", ("TIME",), fill_value=np.nan, zlib=True)  # fill_value=nan otherwise defaults to max
+
+        ncVarOut[:] = altitude_deg
+        ncVarOut.units = "deg"
+        #ncVarOut.setncattr('name', 'sun altitude')
+        ncVarOut.long_name = 'sun_altitude'
+        ncVarOut.comment = "using http://docs.pysolar.org/en/latest/ v0.8 get_altitude"
+
+        if 'SOLAR' in ds.variables:
+            ncVarOut = ds.variables["SOLAR"]
+        else:
+            ncVarOut = ds.createVariable("SOLAR", "f4", ("TIME",), fill_value=np.nan, zlib=True)  # fill_value=nan otherwise defaults to max
+
+        ncVarOut[:] = rad
+        ncVarOut.units = "W/m2"
+        #ncVarOut.setncattr('name', 'extraterrestrial_irrad celestial incoming solar radiation')
+        ncVarOut.long_name = 'incoming_solar_radiation'
+        ncVarOut.comment = "using http://docs.pysolar.org/en/latest/ v0.8 extraterrestrial_irrad() with incoming = 1361 W/m^2"
+
+        if 'ePAR' in ds.variables:
+            ncVarOut = ds.variables["ePAR"]
+        else:
+            ncVarOut = ds.createVariable("ePAR", "f4", ("TIME",), fill_value=np.nan, zlib=True)  # fill_value=nan otherwise defaults to max
+
+        ncVarOut[:] = par
+        ncVarOut.units = "umol/m^2/s"
+        #ncVarOut.setncattr('name', 'global_irradiance_overcast solar radiation')
+        ncVarOut.long_name = 'incoming_solar_radiation'
+        ncVarOut.long_name = 'incoming_solar_radiation converted to PAR (x2.114) attenuated by depth'
+        ncVarOut.comment = "using http://docs.pysolar.org/en/latest/ v0.8 extraterrestrial_irrad() with incoming = 1361 W/m^2, x 2.114, kd = 0.04"
+
+        # rad = [global_irradiance_overcast(lat, lon, d, 1361, temperature=10) for d in dt_utc]
+        #
+        # ncVarOut = ds.createVariable("SURFACE", "f4", ("TIME",), fill_value=np.nan, zlib=True)  # fill_value=nan otherwise defaults to max
+        # ncVarOut[:] = rad
+        # ncVarOut.units = "W/m2"
+        # #ncVarOut.setncattr('name', 'global_irradiance_overcast solar radiation')
+        # ncVarOut.long_name = 'global_irradiance_overcast'
+        # ncVarOut.comment = "using http://docs.pysolar.org/en/latest/ v0.8 global_irradiance_overcast() with incoming = 1361 W/m^2"
+
+        # update the history attribute
+        try:
+            hist = ds.history + "\n"
+        except AttributeError:
+            hist = ""
+
+        ds.setncattr('history', hist + datetime.utcnow().strftime("%Y-%m-%d") + " : added incoming radiation")
+
+        ds.close()
+
+    return out_files
 
 
 if __name__ == "__main__":
-    add_solar(sys.argv[1])
+    add_solar(sys.argv[1:])
