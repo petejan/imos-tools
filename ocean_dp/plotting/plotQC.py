cd0466711903cc2a7efe062d8b86250683b3f2b2
Peter Jansen <peter.jansen@csiro.au>
2020-05-14 12:28:14 +1000

    Update plotQC.py
    improved more friendly version for plotting all QC flags with data

--- ocean_dp/plotting/plotQC.py
+++ ocean_dp/plotting/plotQC.py
@@ -16,34 +16,74 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+import xarray as xr
+
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 import seaborn as sns
 
+from pandas.plotting import register_matplotlib_converters
+
 import sys
-import xarray as xr
+import os
 
-plt.style.use('seaborn-darkgrid')
-sns.set_context("paper")
 
-fn = sys.argv[1]
-#fn = '/Users/pete/cloudstor/SOTS-Temp-Raw-Data/SOFS-7.5-2018/netCDF/IMOS_ABOS-SOTS_TIP_20180801_SOFS_FV01_SOFS-7.5-2018-Starmon-mini-4047-40m_END-20190331_C-20200429.nc'
+def plot_all(files):
+    register_matplotlib_converters()
+    plt.style.use('seaborn-darkgrid')
+    sns.set_context("paper")
 
-DS = xr.open_dataset(fn)
+    pp = PdfPages(os.path.join(os.path.dirname(files[0]), "batch-qc.pdf"))
 
-pp = PdfPages(fn + "-qc.pdf")
+    for f in files:
+        print("file ", f)
+        do_plot(f)
 
-ax1 = plt.subplot(2, 1, 1)
-plt.plot(DS.TIME, DS.TEMP)
+        pp.savefig()
+        plt.close()
 
-ax2 = plt.subplot(2, 1, 2, sharex=ax1)
-aux = DS.TEMP.ancillary_variables
-a_vars = aux.split(" ")
-for f in a_vars:
-    varn = f.split("_")
-    plt.plot(DS.TIME, DS.variables[f], label=varn[-1])
+    pp.close()
 
-plt.legend(prop={'size': 6})
-pp.savefig()
-pp.close()
+
+def plot(fn):
+    register_matplotlib_converters()
+    plt.style.use('seaborn-darkgrid')
+    sns.set_context("paper")
+
+    pp = PdfPages(fn + "-qc.pdf")
+
+    do_plot(fn)
+
+    pp.savefig()
+    pp.close()
+    plt.close()
+
+
+def do_plot(fn):
+
+    #fn = sys.argv[1]
+    #fn = '/Users/pete/cloudstor/SOTS-Temp-Raw-Data/SOFS-7.5-2018/netCDF/IMOS_ABOS-SOTS_TIP_20180801_SOFS_FV01_SOFS-7.5-2018-Starmon-mini-4047-40m_END-20190331_C-20200429.nc'
+
+    DS = xr.open_dataset(fn)
+
+    ax1 = plt.subplot(2, 1, 1)
+    plt.plot(DS.TIME, DS.PAR)
+    plt.title(DS.deployment_code + " - " + DS.instrument_model + ":" + DS.instrument_serial_number + " @ " + str(DS.instrument_nominal_depth), {'fontsize': 8})
+
+    ax2 = plt.subplot(2, 1, 2, sharex=ax1)
+    aux = DS.PAR.ancillary_variables
+    a_vars = aux.split(" ")
+    for f in sorted(set(a_vars)):
+        print('aux var', f)
+        varn = f.split("_")
+        plt.plot(DS.TIME, DS.variables[f], label=varn[-1])
+    plt.ylim(0, 9)
+
+    plt.legend(prop={'size': 6})
+
+    DS.close()
+
+
+if __name__ == "__main__":
+    plot_all(sys.argv[1:])
 
