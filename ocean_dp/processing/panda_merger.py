1c07d8cc57efd37a82324a47ed9ea9081fbc1a85
bweeding <57697604+bweeding@users.noreply.github.com>
2020-05-29 14:27:05 +1000

    Update panda_merger.py

--- ocean_dp/processing/panda_merger.py
+++ ocean_dp/processing/panda_merger.py
@@ -30,33 +30,40 @@ from matplotlib.ticker import PercentFormatter
 from sigfig import round
 import pandas as pd
 
+# import two netcdf
 nc1 = Dataset('IMOS_ABOS-SOTS_COPST_20180801_SOFS_FV00_SOFS-7.5-2018-SBE37SMP-ODO-RS232-03715971-200m_END-20190324_C-20200401.nc',mode='r')
 
 nc2 = Dataset('IMOS_ABOS-SOTS_T_20180801_SOFS_FV00_SOFS-7.5-2018-Starmon-mini-4048-45m_END-20190331_C-20200401.nc',mode='r')
 
-df1 = pd.DataFrame({'TIME':np.array(nc1.variables['TIME'][:]),'TEMP':np.array(nc1.variables['TEMP'][:])})
+# convert their time and temp data into dataframes
+df1 = pd.DataFrame({'TIME':np.array(nc1.variables['TIME'][:]),'TEMP_200':np.array(nc1.variables['TEMP'][:])})
 
-df1_time = np.array(nc1.variables['TIME'][:])
+df2 = pd.DataFrame({'TIME':np.array(nc2.variables['TIME'][:]),'TEMP_45':np.array(nc2.variables['TEMP'][:])})
 
-df2 = pd.DataFrame({'TIME':np.array(nc2.variables['TIME'][:]),'TEMP':np.array(nc2.variables['TEMP'][:])})
-
-# convert datenums to datetimes
-
-pd.to_timedelta(df1['TIME'],unit='D')+dt(1950,1,1)
-
+# convert the times from days since 01-01-1950 to a datetime object
 df1['TIME']=pd.to_timedelta(df1['TIME'],unit='D')+dt(1950,1,1)
 
-df1a=df1.set_index('TIME')
+df2['TIME']=pd.to_timedelta(df2['TIME'],unit='D')+dt(1950,1,1)
 
-# resample hourly 
+# index the dataframes by time
+df1=df1.set_index('TIME')
 
-df1h=df1a.resample('H',base=0.5).mean()
-
-df1h.index = df1h.index + pd.Timedelta('30 min')
-
+df2=df2.set_index('TIME')
 
 
-# merge
+# resample the data, calculating the mean over hourly periods, starting on the half hour
+df1=df1.resample('H',base=0.5).mean()
+
+df2=df2.resample('H',base=0.5).mean()
+
+# reset the labels so they read the hour in the centre of the averaging period
+df1.index = df1.index + pd.Timedelta('30 min')
+
+df2.index = df2.index + pd.Timedelta('30 min')
+
+
+# combine the two dataframes based on their time indicies, recording nan if one sensor doesn't have a reading for that timestamp
+total_df = pd.concat([df1,df2], join='outer', axis=1)
 
 
 
