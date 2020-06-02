b43195f3ff55166fec8847ce5980cfeb035011a8
bweeding <57697604+bweeding@users.noreply.github.com>
2020-06-02 16:13:46 +1000

    netcdf_to_df progress

--- ocean_dp/processing/netcdf_to_df.py
+++ ocean_dp/processing/netcdf_to_df.py
@@ -49,22 +49,93 @@ def netcdf_to_df(target_file):
     # fill the dataframe from the netcdf, variable by variable
     for cur_var in vars_to_transfer:
         
-        df[cur_var] = np.array(nc.variables[cur_var])
-        
-    # convert time into a datetime object, this is optional, and not needed to continue in the IMOS format
-    #df['TIME']=pd.to_timedelta(df['TIME'],unit='D')+dt(1950,1,1)
+        df[cur_var] = np.array(nc.variables[cur_var])    
     
-    # index the dataframe by time        
-    df = df.set_index('TIME')
+    # store deployment times in attributes
+    df.attrs['time_deployment_start'] = nc.time_deployment_start
+    df.attrs['time_deployment_end'] = nc.time_deployment_end
     
     # extract the column names
     col_names = list(df.columns)
             
     # append the nominal depth to all column names
-    df.columns = [x.replace('quality_control',str(nc.variables['NOMINAL_DEPTH'][0])+'_quality_control') if 'quality_control' in x else x + '_' + str(nc.variables['NOMINAL_DEPTH'][0]) for x in col_names]        
-           
+    df.columns = [x.replace('quality_control',str(nc.variables['NOMINAL_DEPTH'][0])+'_quality_control') if 'quality_control' in x else x if 'TIME' in x else x + '_' + str(nc.variables['NOMINAL_DEPTH'][0]) for x in col_names]        
+    
+    nc.close()
+       
+    return df
+    
+    
+# =============================================================================
+# Takes 
+# =============================================================================
+        
+def combine_df(target_dfs):
+    
+    # for each of the dataframes in the list provided
+    for cur_df in target_dfs:
+        
+        # make a copy of the current dataframe to modify and combine
+        df = cur_df.copy()
+        
+        # convert the IMOS format times to datetime 
+        df['TIME']=pd.to_timedelta(df['TIME'],unit='D')+dt(1950,1,1)
+        
+        # index the dataframe by time - for some reason this makes the df very slow to visually open and navigate!?  
+        df = df.set_index('TIME')
+        
+        # extract and convert deployment times to datetime 
+        start_time = dt.strptime(df.attrs['time_deployment_start'],'%Y-%m-%dT%H:%M:%SZ')
+        end_time = dt.strptime(df.attrs['time_deployment_end'],'%Y-%m-%dT%H:%M:%SZ')
+        
+        # trim the df to only include in water data
+        df = df.drop(df[(df.index < start_time) | (df.index > end_time)].index)
+        
+        # resamples using the max method, to create a df of the correct dimensions to fill
+        df_to_fill = df.resample('H',base=0.5).max()
         
         
+        # gets list of column names
+        col_names = list(df.columns)
+        
+        # makes a list of non qc column names
+        col_names_no_qc = [x for x in col_names if 'quality_control' not in x]
+        
+        # for each of the time series data columns
+        for cur_col in col_names_no_qc:
+            
+            # sets the value of non qc data to nan if the corresponding qc value is not satisfactory (0,1,2,7 at the moment)
+            df.loc[(df[cur_col+'_quality_control'] > 2) & (df[cur_col+'_quality_control'] != 7), cur_col] = np.nan
+            
+            # extracts the time series data
+            dS = pd.Series(df[cur_col])
+            
+            # makes a copy for bin counting
+            dS_1s = dS.copy()
+            
+            dS_1s[:] = 1
+            
+            # resamples the series, interpoling linearly
+            dS_resampled = dS.resample('H',base=0.5).interpolate()
+            
+            # count how many data points are in each shoulder bin
+            dS_bin_counts = dS_1s.resample('H',base=0.5).sum()
+            
+            # fill the interpolated data back into the dataframe
+            df_to_fill[cur_col] = dS_resampled
+            
+            # give any interpolated point without any data within its hour window a qc code of 7
+            df_to_fill.loc[dS_bin_counts==0,[cur_col+'_quality_control']] = 7
+            
+        # shift the timestamps to the middle of the hour sampling period
+        df_to_fill.index = df_to_fill.index + pd.Timedelta('30 min')
+        
+
+        
+        
+        
+
+            
         
         
         
