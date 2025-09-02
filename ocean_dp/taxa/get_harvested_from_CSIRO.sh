8ed3e9ed7c80404eb876a2a75ee5b3fc085defdb
Peter Jansen <peter.jansen@csiro.au>
2025-09-02 13:37:43 +1000

    SOFS-14 deployment date fix

---
+++ ocean_dp/taxa/get_harvested_from_CSIRO.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+curl -o bgc_phyto_raw.csv "http://imos-data.s3-website-ap-southeast-2.amazonaws.com/IMOS/BGC_DB/harvested_from_CSIRO/bgc_phyto_raw.csv"
+curl -o bgc_trip.csv "http://imos-data.s3-website-ap-southeast-2.amazonaws.com/IMOS/BGC_DB/harvested_from_CSIRO/bgc_trip.csv"
+curl -o phytoinfo.csv "http://imos-data.s3-website-ap-southeast-2.amazonaws.com/IMOS/BGC_DB/harvested_from_CSIRO/phytoinfo.csv" 
+
+sqlite3 phyto_bgc.sqlite ".import --csv bgc_phyto_raw.csv bgc_phyto_raw"
+sqlite3 phyto_bgc.sqlite ".import --csv bgc_trip.csv bgc_trip"
+sqlite3 phyto_bgc.sqlite ".import --csv phytoinfo.csv phytoinfo"
+
+
+sqlite3 phyto_bgc.sqlite "ALTER TABLE phytoinfo ADD WORMS_APHIA_ID TEXT"
+sqlite3 phyto_bgc.sqlite "ALTER TABLE phytoinfo ADD FAMILY TEXT"
+sqlite3 phyto_bgc.sqlite "ALTER TABLE phytoinfo ADD GENUS TEXT"
+sqlite3 phyto_bgc.sqlite "ALTER TABLE phytoinfo ADD SPECIES TEXT"
+
+python -m ocean_dp.taxa.phyto_sqlite_2_netCDF
+
+python -m ocean_dp.attribution.addAttributes phyto_bgc.sqlite.nc metadata/imos.metadata.csv metadata/sots.metadata.csv metadata/variable.metadata.csv
+python -m ocean_dp.attribution.addAttributes phyto_bgc.sqlite.nc phyto.metadata.csv
+python -m ocean_dp.attribution.format_attributes phyto_bgc.sqlite.nc
+python -m ocean_dp.file_name.imosNetCDFfileName phyto_bgc.sqlite.nc
+
+
+
